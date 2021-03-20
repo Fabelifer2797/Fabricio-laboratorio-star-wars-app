2fd3cd45f7d834516b93430373a04565b883879c
ema11412 <ema11412@hotmail.com>
2021-03-19 18:21:15 -0600

    Add test

---
+++ src/test/java/test1.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import org.testng.Assert;
+
+
+public class test1 {
+
+    @Test
+    public void case1() {
+        String result = "Test";
+        Assert.assertEquals(result, "Test");
+    }
+}
